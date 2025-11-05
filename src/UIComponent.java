d72e839d5ccb5ea47e17a37a679015fb403ca6b9
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-11-05 13:47:52 +0500

    Implement Observer pattern in UIComponent class

--- src/UIComponent.java
+++ src/UIComponent.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class UIComponent {
+public class UIComponent implements Observer {
+    @Override
+    public void update(String eventType, String text) {
+        if ("textChanged".equals(eventType) || "formatApplied".equals(eventType)) {
+            System.out.println("UI: " + text);
+        }
+    }
 }
