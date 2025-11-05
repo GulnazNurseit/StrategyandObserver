4d04d745b4ba88c5fd40797d1ab5a05303509a57
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-11-05 13:34:23 +0500

    Implement ColorTextFormat class with formatting methods

--- src/ColorTextFormat.java
+++ src/ColorTextFormat.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public class ColorTextFormat implements TextFormatStrategy {
+    private String colorCode;
+    private String colorName;
 
-public class ColorTextFormat {
+    public ColorTextFormat(String colorCode, String colorName) {
+        this.colorCode = colorCode;
+        this.colorName = colorName;
+    }
+
+    @Override
+    public String format(String text) {
+        return colorCode + text + "\u001B[0m";
+    }
+
+    @Override
+    public String getName() {
+        return colorName;
+    }
 }
